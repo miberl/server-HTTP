fe4a87abdd179b9d9ba625ce9ad62140f83897ad
Michael <m.berlingieri02@gmail.com>
2020-11-04 09:38:31 +0100

    Second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,31 +6,61 @@ import java.net.Socket;
 
 public class Main {
     public static Socket ss;
+    public static BufferedReader br;
     public static PrintWriter bw;
-    public static String pagina;
 
     public static void main(String[] args) throws IOException, IOException {
 
         int PORTA = 2000;  // porta di ascolto
         ServerSocket S;
-        pagina = "<html><head><title>FUNZIONA</title></head><body><h1>Ciao a tutti belli e brutti</h1></body></html>";
         S = new ServerSocket(PORTA);
-        while (true){
+
+        while (true) {
             ss = S.accept();
-            BufferedReader br = new BufferedReader(new InputStreamReader(ss.getInputStream()));
+            br = new BufferedReader(new InputStreamReader(ss.getInputStream()));
             bw = new PrintWriter(new OutputStreamWriter(ss.getOutputStream()), true);
-            String riga = null;
-            while (!(riga = br.readLine()).equals("")) ;
-            sendpage();
+            readRequest();
             ss.close();
         }
 
     }
 
-    public static void sendpage() throws IOException {
-        bw.println("HTTP/1.1 200 OK");
-        bw.println("Content length: " + pagina.getBytes().length);
-        bw.println();
-        bw.println(pagina);
+
+    public static void readRequest() throws IOException {
+        String riga[] = br.readLine().split(" ");
+        if (riga.length == 3) {
+            switch (riga[0]) {
+                case "GET":
+                    get(riga[1]);
+                    break;
+                default:
+                    break;
+            }
+        }
+
     }
+
+    public static void get(String directory) throws IOException {
+        if (directory.equals("/"))
+            directory = "/index.html";
+        directory = "www" + directory;
+        System.out.println(directory);
+        sendpage(directory);
+    }
+
+
+    public static void sendpage(String directory) throws IOException {
+        String content = "";
+        BufferedReader fr = new BufferedReader(new FileReader(directory));
+        String riga;
+        while ((riga = fr.readLine()) != null)
+            content += riga;
+
+        bw.println("HTTP/1.1 200 OK");
+        bw.println("Content length: " + content.getBytes().length);
+        bw.println("Content Type: text/html");
+        bw.println();
+        bw.println(content);
+    }
+
 }
