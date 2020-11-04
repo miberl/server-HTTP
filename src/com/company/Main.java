52f54e2a54fef34b181ec404dcde48459f034ce0
Michael <m.berlingieri02@gmail.com>
2020-11-04 09:53:35 +0100

    Working FnF 404 Answers all text file requests

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -44,8 +44,12 @@ public class Main {
         if (directory.equals("/"))
             directory = "/index.html";
         directory = "www" + directory;
-        System.out.println(directory);
-        sendpage(directory);
+        if (new File(directory).exists())
+            sendpage(directory);
+        else {
+            fileNotFoundError();
+        }
+
     }
 
 
@@ -57,6 +61,20 @@ public class Main {
             content += riga;
 
         bw.println("HTTP/1.1 200 OK");
+        bw.println("Content length: " + content.getBytes().length);
+        bw.println("Content Type: text/html");
+        bw.println();
+        bw.println(content);
+    }
+
+    public static void fileNotFoundError() throws IOException {
+        String content = "";
+        BufferedReader fr = new BufferedReader(new FileReader("www/error.html"));
+        String riga;
+        while ((riga = fr.readLine()) != null)
+            content += riga;
+
+        bw.println("HTTP/1.1 404 Not Found");
         bw.println("Content length: " + content.getBytes().length);
         bw.println("Content Type: text/html");
         bw.println();
